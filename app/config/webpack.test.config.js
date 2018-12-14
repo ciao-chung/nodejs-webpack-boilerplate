9230849b18734f61e17925601f45f1424c352064
Ciao Chung <ciao0958@gmail.com>
2018-12-14 21:28:07 +0800

    fix: remove not use code

--- app/config/webpack.test.config.js
+++ app/config/webpack.test.config.js
@@ -4,18 +4,5 @@ const merge = require('webpack-merge')
 module.exports = () => {
   return merge(baseConfig(), {
     devtool: '#inline-source-map',
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env'],
-            },
-          },
-        },
-      ],
-    },
   })
 }
